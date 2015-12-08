7c90138648c20b433fc2311f3eb52d6488d8da8f
nilkanthr <nilkanth.rachote@gmail.com>
2015-12-08 22:59:07 +0530

    First commit

---
+++ src/test/java/com/eminence/webservices/stub/StashStub.java
@@ -0,0 +1,11 @@
+package com.eminence.webservices.stub;
+
+import javax.servlet.http.HttpServletRequest;
+
+class StashStub implements ServiceStub {
+
+    public String getResponseFor(HttpServletRequest httpServletRequest) {
+        return null;
+    }
+
+}
